3404cfc66e273b2201a936463e9cf7098796c0b7
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-10-17 21:23:59 +0900

    배경 화면 띄우기

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -9,19 +9,31 @@ enum class Direction
 	Left, Right, Up, Down
 };
 
-//1:09
 void shift(Direction d)
 {
 
 }
 
-//1:16
 int main()
 {
-	RenderWindow app(VideoMode(504, 504), "2048");
+	RenderWindow app(VideoMode(504, 504), "2048");	//64*8
+	app.setFramerateLimit(60);
 
-	Texture tblank, t2, t4, t8, t16, t32, t64, t128, t256, t512, t1024, t2048;
-	tblank.loadFromFile("images/background.jpg");
+	Texture t1, t2, t4, t8, t16, t32, t64, t128, t256, t512, t1024, t2048;
+	t1.loadFromFile("images/background.png");
+	t2.loadFromFile("images/2.png");
+	t4.loadFromFile("images/4.png");
+	t8.loadFromFile("images/8.png");
+	t16.loadFromFile("images/16.png");
+	t32.loadFromFile("images/32.png");
+	t64.loadFromFile("images/64.png");
+	t128.loadFromFile("images/128.png");
+	t256.loadFromFile("images/256.png");
+	t512.loadFromFile("images/512.png");
+	t1024.loadFromFile("images/1024.png");
+	t2048.loadFromFile("images/2048.png");
+
+	Sprite s_back(t1), s2(t2), s4(t4), s8(t8), s16(t16), s32(t32), s64(t64), s128(t128), s256(t256), s512(t512), s1024(t1024), s2048(t2048);
 
 	while (app.isOpen())
 	{
@@ -39,6 +51,12 @@ int main()
 			else if (event.type == Event::Closed)
 				app.close();
 		}
+
+		//draw
+		app.clear();
+		app.draw(s_back);
+
+		app.display();
 	}
 
 	return 0;
